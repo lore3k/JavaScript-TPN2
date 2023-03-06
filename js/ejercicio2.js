e8df7ba728160bce037bc4bc217b3748ef2bcc37
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 12:54:27 -0300

    Ejercicio 2 terminado

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,7 +1,7 @@
 /* 
 Dificultad:  🟢🟡
 
-2- Escribir un programa que solicite una nota (número) de 0  a 10. 
+2- Escribir un programa que solicite una nota (número) de 0 a 10. 
 Luego mostrar la calificación en un alert según los siguientes rangos de nota:
 
 0-2: Muy deficiente
@@ -14,3 +14,39 @@ Luego mostrar la calificación en un alert según los siguientes rangos de nota:
 Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje 
 de “número erróneo”. Si el número ingresado no es válido mostrar el mensaje “Introduce un número válido”.
 */
+
+let nota = prompt("Ingrese una nota del 0 al 10");
+ 
+if(!isNaN(nota)){
+    switch (nota) {
+        case "0":
+        case "1":
+        case "2":
+            alert("Muy deficiente");
+          break;
+        case "3":
+        case "4":
+            alert("Insuficiente");
+            break;
+        case "5":
+        case "6":
+            alert("Suficiente");
+            break;  
+        case "7":
+            alert("Bien");
+            break;    
+        case "8":
+        case "9":
+            alert("Notable");
+            break;  
+        case "10":
+            alert("Sobresaliente");
+            break;  
+        default:
+            alert("Número erróneo, debe ingresar un número del 0 al 10");
+      }
+}
+else{
+    alert("Introduce un número válido");
+}
+  
