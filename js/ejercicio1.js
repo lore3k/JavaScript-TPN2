d7f49872ae2191a24cd06cdad9c77b5150f5ee0c
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 12:25:43 -0300

    testeando

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -3,7 +3,7 @@ Dificultad:  🟢
 
 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar 
 un mensaje que ya puede conducir, si la edad ingresada no es un número válido 
-indicarlo en un mensaje. */
+indicarlo en un mensaje.*/
 
 let edad = parseInt(prompt("Ingrese su edad"));
 
