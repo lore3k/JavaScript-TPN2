870bc36fbd03ea5e2c1501d29845c2be6be1a529
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 15:18:27 -0300

    Enunciado ejercicio 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,18 @@
+/* 
+Dificultad:  🟢🟡🔴
+9- Crea un script que escriba los números del 1 al 500, que indique cuáles son múltiplos de 4 y de 9 y que cada 5 líneas muestre una línea horizontal. Por ejemplo :
+
+1
+2
+3
+4 (Múltiplo de 4)
+5-
+————————————————————-
+
+6
+7
+8 (Múltiplo de 4)
+9 (Múltiplo de 9)
+10
+
+*/
