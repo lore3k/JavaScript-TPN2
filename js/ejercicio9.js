7e18f13a650a67c854e13592ca587449e384b77d
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 15:26:40 -0300

    Ejercicio 9 resuelto

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -18,3 +18,19 @@ líneas muestre una línea horizontal. Por ejemplo :
 10
 
 */
+
+let i, repeat=0;
+
+for(i=1; i<=500; i++){
+    document.write(i);
+    if(i%4==0){
+        document.write(" Múltilplo de 4" );
+    }
+    if(i%9==0){
+        document.write(" Múltiplo de 9 ");
+    }
+    document.write("<br>");
+    if(i%5==0){
+        document.write("<hr>");
+    }
+}
