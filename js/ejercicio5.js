76fc4c6d6a50bb47fc541cb6121883ea766552e2
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 14:20:49 -0300

    Ejercicio 5 resuelto

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -12,3 +12,93 @@ Si lo introducido no es un número deberá indicarse con un alert y volver a pre
 Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
 
 */
+
+let resto = 0;
+let letra = "";
+
+do {
+    let numero = prompt("Introduce tu DNI");
+    if (!isNaN(numero)) {
+        if (numero >= 0 && numero <= 99999999) {
+            resto = numero % 23;
+            switch (resto) {
+                case 0:
+                    letra = "T";
+                    break;
+                case 1:
+                    letra = "R";
+                    break;
+                case 2:
+                    letra = "W";
+                    break;
+                case 3:
+                    letra = "A";
+                    break;
+                case 4:
+                    letra = "G";
+                    break;
+                case 5:
+                    letra = "M";
+                    break;
+                case 6:
+                    letra = "Y";
+                    break;
+                case 7:
+                    letra = "F";
+                    break;
+                case 8:
+                    letra = "P";
+                    break;
+                case 9:
+                    letra = "D";
+                    break;
+                case 10:
+                    letra = "X";
+                    break;
+                case 11:
+                    letra = "B";
+                    break;
+                case 12:
+                    letra = "N";
+                    break;
+                case 13:
+                    letra = "J";
+                    break;
+                case 14:
+                    letra = "Z";
+                    break;
+                case 15:
+                    letra = "S";
+                    break;
+                case 16:
+                    letra = "Q";
+                    break;
+                case 17:
+                    letra = "V";
+                    break;
+                case 18:
+                    letra = "H";
+                    break;
+                case 19:
+                    letra = "L";
+                    break;
+                case 20:
+                    letra = "C";
+                    break;
+                case 21:
+                    letra = "K";
+                    break;
+                case 22:
+                    letra = "E";
+                    break;
+            }
+        alert("Numero: " + numero + ". Letra: " + letra);               
+        }
+        else{
+            alert("Numero erróneo");
+        }
+    }
+    else {
+            alert("No es un número. Ingrese un número válido");
+    }
+} while (numero != undefined);
