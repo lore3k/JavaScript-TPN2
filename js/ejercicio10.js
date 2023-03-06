5d6b528300f8bb98075f8a8ba7bb64cdfdda632f
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 15:32:00 -0300

    Enunciado ejercicio 10

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -5,7 +5,5 @@ Dificultad:  🟢🟡🔴
 y escriba una tabla. Dentro de cada una de las celdas deberá 
 escribirse un número consecutivo en orden descendente. 
 Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
-
-
 */
 
