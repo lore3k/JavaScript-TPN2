dafd9be2f896674ebd8a7cc9cd1bd8b4e31ff399
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 13:00:26 -0300

    Ejercicio 3 planteado

---
+++ js/ejercicio3.js
@@ -0,0 +1,9 @@
+/* 
+Dificultad:  🟢🟡
+ 
+3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. 
+Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+
+Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp
+
+*/
