6d2c8df708e21f663cfe22cb6793ec411da5766d
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 15:31:34 -0300

    Enunciado ejercicio 10

---
+++ js/ejercicio10.js
@@ -0,0 +1,11 @@
+/* 
+Dificultad:  🟢🟡🔴
+
+10- Realiza un script que pida número de filas y columnas 
+y escriba una tabla. Dentro de cada una de las celdas deberá 
+escribirse un número consecutivo en orden descendente. 
+Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+
+
+*/
+
