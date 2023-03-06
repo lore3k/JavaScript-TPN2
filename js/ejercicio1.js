3003a7670e8e8c16c0b39d4cbfba88fbe439f091
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 12:20:11 -0300

    Ejericio 1 terminado

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -4,3 +4,17 @@ Dificultad:  🟢
 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar 
 un mensaje que ya puede conducir, si la edad ingresada no es un número válido 
 indicarlo en un mensaje. */
+
+let edad = parseInt(prompt("Ingrese su edad"));
+
+if(isNaN(edad)||(edad>120)){
+    document.write("Debe ingresar una edad válida");
+}
+else{
+    if(edad>=18){
+        document.write("Puede conducir");
+    }
+    else{
+        document.write("No puede conducir");
+    }
+}
