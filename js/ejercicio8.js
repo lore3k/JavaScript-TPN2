5bcfc06730fd6da84853b6b223cb1885f6bd7512
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 15:11:54 -0300

    Ejercicio 8 resuelto

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -13,3 +13,22 @@ del 1 al número que indique el usuario (no mayor de 50)
 ……
 
 */
+
+let numrepeat = prompt("Introduce numero de repeticiones (debe ser entre 1 y 50): ");
+if (!isNaN(numrepeat)){
+    if (numrepeat>0 && numrepeat<=50) {
+        let repeat,i;
+        for (i=0; i<=numrepeat; i++){
+            for (repeat=1; repeat<=i; repeat++){
+                document.write(repeat);
+            }
+            document.write("<br>");
+        }
+    } 
+    else {
+        alert("El número introducido no es válido, debe ser entre 1 y 50");
+    }
+} 
+else {
+    alert("No has introducido un número");
+}
