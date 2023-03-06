0839bdaaee8dcb550de5614d654d52748f2fbbe9
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 12:34:29 -0300

    Planeteo ejercicio 2

---
+++ js/ejercicio2.js
@@ -0,0 +1,16 @@
+/* 
+Dificultad:  🟢🟡
+
+2- Escribir un programa que solicite una nota (número) de 0  a 10. 
+Luego mostrar la calificación en un alert según los siguientes rangos de nota:
+
+0-2: Muy deficiente
+3-4: Insuficiente
+5-6: Suficiente
+7: Bien
+8-9: Notable
+10: Sobresaliente
+
+Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje 
+de “número erróneo”. Si el número ingresado no es válido mostrar el mensaje “Introduce un número válido”.
+*/
