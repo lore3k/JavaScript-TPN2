8720f17cac35e0d969affa00e3441c8dcb786b05
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 14:39:06 -0300

    Planteo ejercicio 8

---
+++ js/ejercicio8.js
@@ -0,0 +1,15 @@
+/* 
+Dificultad:  🟢🟡
+
+8- Crea script para generar pirámide siguiente con los números 
+del 1 al número que indique el usuario (no mayor de 50) 
+
+1
+12
+123
+1234
+12345
+123456
+……
+
+*/
