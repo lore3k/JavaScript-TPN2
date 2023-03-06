edc95b619db1bae6685fe3539d03d0ef5fb071cb
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 16:32:51 -0300

    Ejercicio 12 terminado

--- js/ejercicio12.js
+++ js/ejercicio12.js
@@ -3,3 +3,10 @@ Dificultad:  🟢🟡
 12- Realiza un script que genere un número aleatorio entre 1 y 99.
 
 */
+let numero = Math.floor((Math.random()*99)+1);
+document.write(numero);
+
+/*
+ Math.random: devuelve un número aleatorio mayor que 0 y menor que 1.
+ Math.floor: redondea al número entero menor.
+*/
