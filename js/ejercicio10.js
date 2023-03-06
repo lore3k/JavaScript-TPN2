f40156af458a9979399666faa8a1b7e588a64721
Lore Ginard <lorenaginard@hotmail.com>
2023-03-06 15:53:58 -0300

    Ejercicio 10 resuelto

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -7,3 +7,19 @@ escribirse un número consecutivo en orden descendente.
 Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
 */
 
+let i,j;
+let filas = parseInt(prompt("Introduce un número de filas"));
+let columnas = parseInt(prompt("Introduce un número de columnas"));
+let resultado = filas * columnas;
+document.write("<table border>");
+for (i=0;i<filas;i++){
+    document.write("<tr>");
+    for (j=0;j<columnas;j++){
+        document.write("<td>");
+        document.write(resultado);
+        resultado--;
+        document.write("</td>");
+    }
+    document.write("</tr>");
+}
+document.write("</table>");
