5617185ff47b7ba2acf6c4b733ff5068efe63634
Lore Ginard <lorenaginard@hotmail.com>
2023-03-07 10:37:32 -0300

    Ejercicio 17 terminado

--- js/ejercicio17.js
+++ js/ejercicio17.js
@@ -4,3 +4,16 @@ Dificultad:  🟢
 17- Realiza un script que muestre la posición de la primera 
 vocal de un texto introducido por teclado.
 */
+
+let texto = prompt("Ingrese un texto");
+let i, caracter;
+texto=texto.toLowerCase();
+
+for(i=0; i<texto.length; i++){
+    caracter = texto.charAt(i);
+    if(caracter==="a" || caracter==="e" || caracter==="i" 
+    || caracter==="o" || caracter==="u"){
+        document.write("La primera vocal está en la posición "+(i+1));
+        break;
+    }
+}
