349d6755306fae5267eba7beb1a0b9546da8fffd
Lore Ginard <lorenaginard@hotmail.com>
2023-03-07 10:26:42 -0300

    Ejercicio 16 resuelto

--- js/ejercicio16.js
+++ js/ejercicio16.js
@@ -3,3 +3,13 @@ Dificultad:  🟢🟡
 16- Realiza un script que pida una cadena de texto y la devuelva al revés. 
 Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
 */
+
+let cadena = prompt("Ingrese una cadena");
+let caracter,i;
+let resultado="";
+
+for(i=cadena.length; i>=0; i--){
+    caracter = cadena.charAt(i);
+    resultado=resultado+caracter;
+}
+document.write(resultado);
