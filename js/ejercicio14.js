50b700f59aaffdc074312ec81f655c6d50239f4c
Lore Ginard <lorenaginard@hotmail.com>
2023-03-07 09:55:56 -0300

    Enunciado ejercicio 14

---
+++ js/ejercicio14.js
@@ -0,0 +1,7 @@
+/* 
+Dificultad:  🟢🟡
+14- Realiza un script que pida una cadena de texto y 
+lo muestre poniendo el signo – entre cada carácter sin usar el 
+método replace. Por ejemplo, si tecleo “hola qué tal”, 
+deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
+*/
