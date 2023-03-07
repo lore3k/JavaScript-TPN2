6731506b8028e8950d1bbff3391e76b01fd317d4
Lore Ginard <lorenaginard@hotmail.com>
2023-03-07 10:14:29 -0300

    Ejercicio 15 terminado

--- js/ejercicio15.js
+++ js/ejercicio15.js
@@ -3,5 +3,19 @@ Dificultad:  🟢🟡
 
 15- Realiza un script que cuente el número de vocales
  que tiene un texto.
-
 */
+
+let texto = prompt("Ingrese un texto");
+let i, contador=0;
+let caracter;
+texto=texto.toLowerCase();
+
+for(i=0; i<texto.length; i++){
+    caracter = texto.charAt(i);
+    if(caracter==="a" || caracter==="e" || caracter==="i" 
+    || caracter==="o" || caracter==="u"){
+        contador++;
+    }
+}
+
+document.write("Hay "+contador+" vocales en el texto");
