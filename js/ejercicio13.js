9f8511e0b30081b0035136ee914daf20d86e49f7
Lore Ginard <lorenaginard@hotmail.com>
2023-03-07 09:51:17 -0300

    Ejercicio 13 resuelto

--- js/ejercicio13.js
+++ js/ejercicio13.js
@@ -5,3 +5,7 @@ Dificultad:  🟢🟡
 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
 
 */
+
+let texto = String(prompt("Introduce una frase: "));
+document.write(texto.toUpperCase());
+
