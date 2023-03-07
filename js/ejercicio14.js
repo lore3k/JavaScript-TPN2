522f9a33382b93c1bfe1c616acbf23731330a271
Lore Ginard <lorenaginard@hotmail.com>
2023-03-07 10:05:55 -0300

    Ejercicio 14 resuelto

--- js/ejercicio14.js
+++ js/ejercicio14.js
@@ -5,3 +5,18 @@ lo muestre poniendo el signo – entre cada carácter sin usar el
 método replace. Por ejemplo, si tecleo “hola qué tal”, 
 deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
 */
+
+let cadena = prompt("Ingrese un texto");
+let i;
+let resultado;
+
+
+for(i=0; i<cadena.length; i++){
+    resultado = cadena.charAt(i);
+    if(i!=cadena.length-1){
+        document.write(resultado+"-");
+    }
+    else{
+        document.write(resultado);
+    }
+}
